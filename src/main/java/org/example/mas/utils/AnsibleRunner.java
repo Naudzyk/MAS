a002f52df1755330b1827bcdd58ae48bf3f60581
Naudzyk <zhenya_nikolaev_1995@list.ru>
2025-11-04 14:42:53 +0800

    "Version Spring-Web 8.5"

--- src/main/java/org/example/mas/utils/AnsibleRunner.java
+++ src/main/java/org/example/mas/utils/AnsibleRunner.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public class AnsibleRunner {
