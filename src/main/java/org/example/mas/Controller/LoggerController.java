d257c7d1d7fe33a39e1cc03d86af8a393c413bf1
Naudzyk <zhenya_nikolaev_1995@list.ru>
2025-11-04 19:05:25 +0800

    "Version Spring-Web 8.8"

--- src/main/java/org/example/mas/Controller/LoggerController.java
+++ src/main/java/org/example/mas/Controller/LoggerController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.mas.Service.Agent.StatusService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,8 @@ public class LoggerController {
 
     private final StatusService statusService;
     private final AgentContainer agentContainer;
+
+
 
 
     @GetMapping()
